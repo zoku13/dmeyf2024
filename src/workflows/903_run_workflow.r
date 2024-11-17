4a4783eaa652f6b9e9402e5ec480047b68801426
zoku13 <zonia.morales.salomon@gmail.com>
2024-11-17 00:04:52 -0300

    903 corr

--- src/workflows/903_run_workflow.r
+++ src/workflows/903_run_workflow.r
@@ -1,11 +1,7 @@
 require("rlang", quietly=TRUE) 
 
 # workflow que voy a correr
-<<<<<<< HEAD
 PARAM <- "src/workflows/918_workflow_base_lagdelta12.r"
-=======
-PARAM <- "src/workflows/918_workflow_base_lugdelta2tend1y2.r"
->>>>>>> 9a1d343148caeb4f7d6064ac96d882d5b89f963f
 
 envg <- env()
 
@@ -18,23 +14,23 @@ correr_workflow <- function( wf_scriptname )
 {
   dir.create( "~/tmp", showWarnings = FALSE)
   setwd("~/tmp" )
-
+  
   # creo el script que corre el experimento
   comando <- paste0( 
-      "#!/bin/bash\n", 
-      "source /home/$USER/.venv/bin/activate\n",
-      "nice -n 15 Rscript --vanilla ",
-      envg$EXPENV$repo_dir,
-      wf_scriptname,
-      "   ",
-      wf_scriptname,
-     "\n",
-     "deactivate\n"
-    )
+    "#!/bin/bash\n", 
+    "source /home/$USER/.venv/bin/activate\n",
+    "nice -n 15 Rscript --vanilla ",
+    envg$EXPENV$repo_dir,
+    wf_scriptname,
+    "   ",
+    wf_scriptname,
+    "\n",
+    "deactivate\n"
+  )
   cat( comando, file="run.sh" )
-
+  
   Sys.chmod( "run.sh", mode = "744", use_umask = TRUE)
-
+  
   system( "./run.sh" )
 }
 #------------------------------------------------------------------------------
