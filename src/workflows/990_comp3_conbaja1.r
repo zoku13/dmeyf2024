315ee37c162889fe38c44eb9b7da64947ac2daac
zoku13 <zonia.morales.salomon@gmail.com>
2024-11-29 14:11:36 +0000

    auto commit 1       20241129 141130

--- src/workflows/990_comp3_conbaja1.r
+++ src/workflows/990_comp3_conbaja1.r
@@ -13,8 +13,8 @@ if( !exists("envg") ) envg <- env()  # global environment
 
 envg$EXPENV <- list()
 envg$EXPENV$bucket_dir <- "~/buckets/b1"
-envg$EXPENV$exp_dir <- "~/buckets/b1/expwSEMI9/"
-envg$EXPENV$wf_dir <- "~/buckets/b1/flowSEMI9/"
+envg$EXPENV$exp_dir <- "~/buckets/b1/expw3ra/"
+envg$EXPENV$wf_dir <- "~/buckets/b1/flow3ra/"
 envg$EXPENV$repo_dir <- "~/dmeyf2024/"
 envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$messenger <- "~/install/zulip_enviar.sh"
